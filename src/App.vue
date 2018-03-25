e339588f705176d12159119cba4cecde40e72e32
Latence132 <alexandre.chaumet@gadz.org>
2018-03-25 21:23:18 +0200

    second commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app" class="ui center aligned container segment">
+    <!--    <HelloWorld/>-->
+      <Todo/>
+
+  </div>
+</template>
+
+<script>
+//  import HelloWorld from './components/HelloWorld'
+import Todo from './components/Todo'
+
+export default {
+  name: 'App',
+  components: {
+    //  HelloWorld
+    Todo
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        {{ msg }}
+        <br/>
+        <div class="ui input" >
+            <input type="text" v-on:keyup.enter="add" v-model="nouvelItem" name="nouvelItem" >
+        </div>
+        <div class="ui list" v-for="item in items" :key="item">
+            <div class="ui right labeled left icon input list" >
+                <i class="tags icon"></i>
+                <input type="text" placeholder="Chose à faire"  :value="item" :name="item" :class="done">
+                <a class="ui tag label"  @click="done(item)" :name="item">
+                     <i class="check icon"></i>
+                </a>
+                <a class="ui tag label" @click="remove(item)" :name="item">
+                   <i class="trash icon"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+name: 'To-do-list',
+    data () {
+        return {
+            nouvelItem: 'Nouvelle chose à faire',
+            msg: 'Welcome to To do list',
+            items: ['JS', 'Vuejs', 'NodeJS']
+        }   
+    },
+    methods: {
+        add(){
+                console.log('appel fct Add');
+                this.items.push(this.nouvelItem)
+        },
+       
+        remove(item){
+            //  supprimé l'item
+            // l'index de l'élément: items.indexof(this.item)
+            // suppréssion dasn le tableau this.items.slice(items.indexof(this.item),1)
+            this.items.splice(this.items.indexOf(item),1);
+        }
+    },
+    computed:{
+         done(item){
+            //  barré la chose faire
+            console.log('appel fct Done');
+            return {  'text-decoration': 'line-through'}
+        },
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+    .done{
+        text-decoration: line-through;
+    }
+</style>
